a081780877e84783236d724ef883bbc54e31b3bd
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 17:24:53 -0400

    add projects and about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Introduction from './Components/Introduction';
+import About from './Components/About';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Introduction />
+      <About />
       <Footer />
     </div>
   );
