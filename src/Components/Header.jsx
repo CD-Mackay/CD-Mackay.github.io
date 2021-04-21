3b520a811a8493bd245849df59e0907f5a031b20
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 17:18:01 -0400

    add header too app

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Header() {
   return (
     <nav id="nav">
-        <div class="nav-links">
-        <a class="nav-link" href="#skills">Skills</a>
-        <a class="nav-link" href="#projects">Projects</a>
-        <a class="nav-link" href="https://www.canva.com/design/DAEUQ21my98/3yUcC9-tWeZzSpLdhaygiQ/view?utm_content=DAEUQ21my98&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="blank">Resume</a>
+        <div className="nav-links">
+        <a className="nav-link" href="#skills">Skills</a>
+        <a className="nav-link" href="#projects">Projects</a>
+        <a className="nav-link" href="https://www.canva.com/design/DAEUQ21my98/3yUcC9-tWeZzSpLdhaygiQ/view?utm_content=DAEUQ21my98&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="blank">Resume</a>
       </div>
       </nav>
   )
