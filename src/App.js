64ba776e649d1148786628dd58aba74857a38bb7
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 17:22:34 -0400

    add introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Introduction from './Components/Introduction';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Introduction />
       <Footer />
     </div>
   );
