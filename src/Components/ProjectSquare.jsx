4622e39064550cd7f8f19b86e0a55b3ae7a4d443
CD-Mackay <connor.mackay@gmail.com>
2021-04-22 14:44:21 -0400

    add urls to project repos

--- src/Components/ProjectSquare.jsx
+++ src/Components/ProjectSquare.jsx
@@ -4,7 +4,7 @@ export default function ProjectSquare(props) {
   return (
     <div class="project-square">
           <img src={props.image} class="project-image" alt="project-screen" />
-          <h3>props.title</h3>
+          <h3>{props.title}</h3>
           <ul>
             <li>{props.description}</li>
             {props.stack}
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -16,30 +16,41 @@ export default function Projects() {
         <ProjectSquare image={caesar} 
                        title={"Caesar Cipher"}
                        description={"Allows for encryption/decryption of short messages using a caesar cipher."}
-                       stack={<li>Users can set encryption and decrpytion key to ensure confidentiality.</li>}/>
+                       stack={<li>Users can set encryption and decrpytion key to ensure confidentiality.</li>}
+                       url={"https://github.com/CD-Mackay/caesar-messaging"}/>
         <ProjectSquare image={fibonacci} 
+                       title={"Fibonacci Generator"}
                         description={"Practice project to implement data visualization"}
-                        stack={<li>React front end, Express API accesses Postgresql database.</li>} />
+                        stack={<li>React front end, Express API accesses Postgresql database.</li>}
+                        url={"https://github.com/CD-Mackay/fibonacci-generator"} />
         <ProjectSquare image={helpmehelpyou} 
+                       title={"HelpmeHelpYou"}
                        description={"Full-Stack Application which connects users for mutual aid based on location."}
                        stack={<li>Built Collaboratively with 
                                 <a href="https://github.com/justinkwanchan" target="blank">Justin Chan
                                 </a> and 
                                 <a href="https://github.com/alan-mak" target="blank">Alan Mak
                                 </a>
-                              </li>}/>
+                              </li>}
+                       url={"https://github.com/alan-mak/HelpMeHelpYou"}/>
         <ProjectSquare image={litevote} 
+                       title={"Litevote"}
                        description={"Jquery/Express application which helps users make group decisions."}
                        stack={<li>Built collaboratively with 
                                 <a href="https://github.com/alan-mak" target="blank">Alan Mak
                                 </a>
-                              </li>}/>
+                              </li>}
+                       url={"https://github.com/alan-mak/LiteVote"}/>
         <ProjectSquare image={recipe} 
+                       title={"Alternative Recipe Format"}
                        description={"Made prior to attending Lighthouse Labs. Alternative format for recipe blogs to make cooking more accessible."}
-                       stack={<li>Simple HTML, CSS with basic Javascript</li>}/>
+                       stack={<li>Simple HTML, CSS with basic Javascript</li>}
+                       url={"https://github.com/CD-Mackay/Alternative-Recipe-Format"}/>
         <ProjectSquare image={scheduler} 
+                       title={"Scheduler"}
                        description={"single page application built for Lighthouse Labs Bootcamp"} 
-                       stack={<li>Built with React, tested through Jest and Storybook</li>}/>
+                       stack={<li>Built with React, tested through Jest and Storybook</li>}
+                       url={"https://github.com/CD-Mackay/scheduler"}/>
                        
       </div>
       </section>
