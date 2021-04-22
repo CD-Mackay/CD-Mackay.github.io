8521143f939cc5d1af1e04f3d3e3e67f3f13ec6b
CD-Mackay <connor.mackay@gmail.com>
2021-04-22 14:24:19 -0400

    add image files

---
+++ src/Components/ProjectSquare.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ProjectSquare(props) {
+  return (
+    <div class="project-square">
+          {/* <img src="images/helpmehelpyou.png" class="project-image" /> */}
+          <h3>props.title</h3>
+          <ul>
+            <li>Full-Stack Application which connects users for mutual aid based on location.</li>
+            <li>Built Collaboratively with <a href="https://github.com/justinkwanchan" target="blank">Justin Chan</a> and <a href="https://github.com/alan-mak" target="blank">Alan Mak</a></li>
+            <li><a href="https://github.com/alan-mak/HelpMeHelpYou"><i class="dark-icon fab fa-github"></i></a></li>
+          </ul>
+        </div>
+  )
+}
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ProjectSquare from './ProjectSquare';
+import caesar from '../Images/caesar.png';
+import fibonacci from '../Images/fibonacci.png';
+import helpmehelpyou from '../Images/helpmehelpyou.png';
+import litevote from '../Images/litevote.png';
+import rails from '../Images/rails.png';
+import recipe from '../Images/recipe.png';
+import scheduler from '../Images/scheduler.png';
 
 export default function Projects() {
   return (
     <section id="projects">
       <h2>Projects</h2>
       <div id="projects-grid">
+        <ProjectSquare />
       </div>
       </section>
   )
