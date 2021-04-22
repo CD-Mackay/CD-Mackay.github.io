ee905e9436f0f7bcc47ee83d367f07da2d7e562a
CD-Mackay <connor.mackay@gmail.com>
2021-04-22 17:30:39 -0400

    add seperator

--- src/Components/ProjectSquare.jsx
+++ src/Components/ProjectSquare.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styles.scss';
 
 
 export default function ProjectSquare(props) {
@@ -7,16 +8,14 @@ export default function ProjectSquare(props) {
     <div class="project-square">
           <img src={props.image} class="project-image" alt="project-screen" />
           <h3>{props.title}</h3>
-          <ul>
+          <ul className="project-list">
             <li>{props.description}</li>
             {props.stack}
-            <li>
-              <a href="https://github.com/alan-mak/HelpMeHelpYou">
+          </ul>
+          <a className="git-link" href="https://github.com/alan-mak/HelpMeHelpYou">
               <FontAwesomeIcon icon={["fab", "github-alt"]} />
               View Project
               </a>
-            </li>
-          </ul>
         </div>
   )
 }
